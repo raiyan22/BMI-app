d87fab0db6e069015b3bb2750f16074f42a5a7fe
raiyan22 <raiyanislam058@gmail.com>
2021-12-16 03:45:29 -0800

    added round_icon_button

---
+++ lib/components/round_icon_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+
+  RoundIconButton({ @required this.icon, @required this.onPressed});
+
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return RawMaterialButton(
+        onPressed: onPressed,
+      elevation: 0.0,
+      child: Icon(icon),
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Colors.blueGrey,
+    );
+  }
+}
